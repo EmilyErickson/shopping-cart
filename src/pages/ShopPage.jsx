9d309a452f2fa6795144b907ba2769404d9ba64a
EmilyErickson <emilyerickson2002@gmail.com>
2024-02-02 16:56:58 -0600

    Move all of the logic to App file in order to lift state up

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,50 +1,25 @@
-import { FetchProducts } from "../components/Products"
 import {ProductCard} from "../components/ProductCard"
-import { useState } from "react"
+import PropTypes from 'prop-types';
 
 
-function ShopPage() {
-    let productData = FetchProducts()
-
-    const [cartItems, setCartItems] = useState([])
-
-
-    function addToCart(product, amount) {
-        const existingItem = cartItems.find((item) => item.id === product.id);
-
-        if(existingItem) {
-            if(amount === 0) {
-                removeFromCart(product)
-            }
-            setCartItems((prevItems) =>
-            prevItems.map((item) =>
-            item.id === product.id ? {...item, quantity: amount} : item
-            )
-            );
-        } else {
-
-            setCartItems((prevItems) => 
-                 [...prevItems, {...product, quantity: amount}]
-                )
-        }
-    }
-
-    function removeFromCart(product) {
-        let newCartItems = cartItems.filter(item => item.id !== product.id);
-        setCartItems(newCartItems)
-    }
-
-
+function ShopPage({productData, addToCart, cartItems, removeFromCart}) {
     return (
         <div>
             <h1>Store</h1>
             <ul>
             {productData.map((item) => (
-                <ProductCard key={item.id} item={item} addToCart={addToCart} cartItems={cartItems}/>
+                <ProductCard key={item.id} item={item} addToCart={addToCart} cartItems={cartItems} removeFromCart={removeFromCart}/>
             ))}
             </ul>
         </div>
     )
 }
+
+ShopPage.propTypes = {
+    productData: PropTypes.array,
+    addToCart: PropTypes.func,
+    cartItems: PropTypes.array,
+    removeFromCart: PropTypes.func,
+  }
 
 export {ShopPage}
