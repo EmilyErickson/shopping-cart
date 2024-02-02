41c96082ceedc7f0ac0b0e4f2aa88e37937d28b8
EmilyErickson <emilyerickson2002@gmail.com>
2024-02-02 16:58:13 -0600

    Add logic for adding and removing from cart and cartItems in order to lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import {HomePage} from './pages/HomePage';
 import {ShopPage} from './pages/ShopPage';
 import { Navbar } from './components/Navbar';
 import Cart from './components/Cart';
+import { FetchProducts } from './components/Products';
+import Checkout from './components/Checkout';
+import {ErrorPage} from "./pages/ErrorPage";
 
 function App() {
+  let productData = FetchProducts()
+
+  const [cartItems, setCartItems] = useState([])
+
+
+  function addToCart(product, amount) {
+      const existingItem = cartItems.find((item) => item.id === product.id);
+
+      if(existingItem) {
+          if(amount === 0) {
+              removeFromCart(product)
+          }
+          setCartItems((prevItems) =>
+          prevItems.map((item) =>
+          item.id === product.id ? {...item, quantity: amount} : item
+          )
+          );
+      } else {
+
+          setCartItems((prevItems) => 
+               [...prevItems, {...product, quantity: amount}]
+              )
+      }
+  }
+
+  function removeFromCart(product) {
+      let newCartItems = cartItems.filter(item => item.id !== product.id);
+      setCartItems(newCartItems)
+      console.log(newCartItems)
+  }
+
 
   return (
     <div>
@@ -12,8 +47,10 @@ function App() {
       <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/shop" element={<ShopPage productData={productData} addToCart={addToCart} cartItems={cartItems} removeFromCart={removeFromCart}/>} />
+        <Route path="/cart" element={<Cart cartItems={cartItems} removeFromCart={removeFromCart}/>} />
+        <Route path="/checkout" element={<Checkout cartItems={cartItems} />} />
+        <Route path="/*" element={<ErrorPage />} />
       </Routes>
       </div>
     </div>
