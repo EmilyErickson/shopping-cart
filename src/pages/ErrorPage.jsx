33e2209ad8e1fe4682d719dcbdd2941f9cb6e21f
EmilyErickson <emilyerickson2002@gmail.com>
2024-02-05 17:57:33 -0600

    Import style sheet

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom"
+import "./errorPage.css"
+
 
 function ErrorPage() {
     return (
-        <div>
-      <h1>{"Oh no, this route doesn't exist!"}</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
-        </div>
+      <div className="error-page">
+        <h1>{"Oh no, this route doesn't exist!"}</h1>
+        <Link className="home-link" to="/">
+          You can go back to the home page by clicking here, though!
+        </Link>
+      </div>
     )
 }
 
