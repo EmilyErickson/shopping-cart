a09fb606b37990482062f15860f3ebea0d9fcb30
EmilyErickson <emilyerickson2002@gmail.com>
2024-02-06 19:54:22 -0600

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css"
+
+function Footer() {
+  return (
+      <div className="footer">
+        <h3 className="credits">Store items from <a href="https://fakestoreapi.com/">Fake Store API</a> </h3>
+      </div>
+  );
+}
+
+
+export {Footer}
